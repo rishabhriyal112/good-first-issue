54c22fd0a3548f965571f4674baba4a07797c07b
Rishabh <81256217+rishabhriyal112@users.noreply.github.com>
2025-08-10 20:49:46 +0530

    Update [slug].vue
    Signed-off-by: Rishabh <81256217+rishabhriyal112@users.noreply.github.com>

--- pages/language/[slug].vue
+++ pages/language/[slug].vue
@@ -1,24 +1,40 @@
 <template>
   <div class="p-4 w-full">
-    <RepoBox v-for="repo in repositories" :key="repo.id" :repo="repo" />
+    <div v-if="repositories.length === 0" class="text-center text-gray-500 py-8">
+      No repositories found
+    </div>
+    <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <RepoBox 
+        v-for="repo in repositories" 
+        :key="repo.id" 
+        :repo="repo" 
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import Repositories from '~/data/generated.json'
 import Tags from '~/data/tags.json'
 
 const route = useRoute()
 
-const repositories = Repositories.filter(repository => repository.slug === route.params.slug)
+const repositories = computed(() => 
+  Repositories.filter(repository => repository.slug === route.params.slug)
+)
 
-const tag = Tags.find(t => t.slug === route.params.slug)
+const tag = computed(() => 
+  Tags.find(t => t.slug === route.params.slug)
+)
 
 useHead({
-  title: `${tag.language} | Good First Issue`,
+  title: computed(() => `${tag.value?.language || 'Unknown'} | Good First Issue`),
   meta: [{
     name: 'description',
-    content: `Curated list of issues in ${tag.language} from popular open-source projects that you can easily fix.`
+    content: computed(() => 
+      `Curated list of issues in ${tag.value?.language || 'this language'} from popular open-source projects that you can easily fix.`
+    )
   }]
 })
 </script>
